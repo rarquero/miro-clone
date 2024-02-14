dfb22e89a7e8654ed536feaec7c39a23377402d3
Russell <rquicho94@gmail.com>
2024-02-13 20:35:01 -0800

    Added API route with convex

--- convex/board.ts
+++ convex/board.ts
@@ -1,6 +1,19 @@
 import { v } from "convex/values";
 import { mutation } from "./_generated/server";
 
+const images = [
+    "/placeholders/1.svg",
+    "/placeholders/2.svg",
+    "/placeholders/3.svg",
+    "/placeholders/4.svg",
+    "/placeholders/5.svg",
+    "/placeholders/6.svg",
+    "/placeholders/7.svg",
+    "/placeholders/8.svg",
+    "/placeholders/9.svg",
+    "/placeholders/10.svg",
+];
+
 export const create = mutation({
     args: {
         orgId: v.string(),
@@ -12,5 +25,17 @@ export const create = mutation({
         if (!identity) {
             throw new Error("Unauthorized");
         }
+
+        const randomImage = images[Math.floor(Math.random() * images.length)]
+
+        const board = await ctx.db.insert("boards", {
+            title: args.title,
+            orgId: args.orgId,
+            authorId: identity.subject,
+            authorName: identity.name!,
+            imageUrl: randomImage,
+        });
+
+        return board;
     }
 })

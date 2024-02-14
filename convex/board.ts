44154172a439a8660d948029f7e6295b09d29225
Russell <rquicho94@gmail.com>
2024-02-13 20:29:37 -0800

    Added placeholder images and created authorization

---
+++ convex/board.ts
@@ -0,0 +1,16 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+
+export const create = mutation({
+    args: {
+        orgId: v.string(),
+        title: v.string(),
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Unauthorized");
+        }
+    }
+})
